894bad0c464fe1bf117cb8b8addb5ce014c318a5
Moaze Bahloul <moaze.bahloul@gmail.com>
2024-12-04 20:11:16 +1100

    improved day3

--- src/day_3.rs
+++ src/day_3.rs
@@ -9,7 +9,7 @@ impl Puzzle for Day3 {
         3
     }
     fn part_a(data: String) -> String {
-        let regex = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+        let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
         regex
             .captures_iter(&data)
             .map(|x| {
@@ -20,38 +20,32 @@ impl Puzzle for Day3 {
             .to_string()
     }
     fn part_b(data: String) -> String {
-        let do_regex = Regex::new(r"do\(\)").unwrap();
-        let do_idxs = do_regex.find_iter(&data).map(|x| (x.start(), true));
-
-        let dont_regex = Regex::new(r"don't\(\)").unwrap();
-        let dont_idxs = dont_regex.find_iter(&data).map(|x| (x.start(), false));
-
-        let mut valid_idxs = [(0 as usize, true)]
-            .into_iter()
-            .chain(do_idxs)
-            .chain(dont_idxs)
-            .collect::<Vec<_>>();
-        valid_idxs.sort_by(|a, b| a.0.cmp(&b.0));
-        let valid_idxs = valid_idxs.windows(2).collect::<Vec<_>>();
-
-        let mul_regex = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
-        let mul_idxs = mul_regex.captures_iter(&data).map(|x| {
-            let (_, [a, b]) = x.extract();
-            (x.get(0).unwrap().start(), [a, b])
-        });
-
-        mul_idxs
-            .filter(|(i, _)| {
-                valid_idxs
-                    .iter()
-                    .find(|w| {
-                        let is_valid = w[0].1;
-                        let range = w[0].0..w[1].0;
-                        is_valid && range.contains(&i)
-                    })
-                    .is_some()
+        let regex = Regex::new(r"do\(\)|mul\((\d+),(\d+)\)|don't\(\)").unwrap();
+        let mut valid = true;
+        regex
+            .captures_iter(&data)
+            .map(|x| {
+                let m = x.get(0).unwrap().as_str();
+                if m == "do()" {
+                    valid = true;
+                } else if m == "don't()" {
+                    valid = false;
+                }
+                if valid {
+                    x.get(1)
+                        .map(|x_| x_.as_str())
+                        .unwrap_or_else(|| "0")
+                        .parse::<i32>()
+                        .unwrap()
+                        * x.get(2)
+                            .map(|x_| x_.as_str())
+                            .unwrap_or_else(|| "0")
+                            .parse::<i32>()
+                            .unwrap()
+                } else {
+                    0
+                }
             })
-            .map(|(_, [a, b])| a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap())
             .sum::<i32>()
             .to_string()
     }
